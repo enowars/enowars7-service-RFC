1e32414a663d0700f13eb4bc1072fba91825d63b
maxiibon <maxerkel@web.de>
2023-06-06 12:08:40 +0200

    changed interface for gunicorn

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -6,4 +6,4 @@
 #/etc/init.d/nginx start
 flask --app managedServiceProvider init-db
 #gunicorn -w 4 -b '127.0.0.1:6969' 'managedServiceProvider:create_app()'
-gunicorn -w 4 -b '127.0.0.1:5000' 'managedServiceProvider:create_app()'
+gunicorn -w 4 -b '0.0.0.0:5000' 'managedServiceProvider:create_app()'
