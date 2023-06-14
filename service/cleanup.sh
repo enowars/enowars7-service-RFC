a6b4e508ad511b64751681213f9b6587787a015c
Maximilian Erkel <maxerkel@web.de>
2023-06-14 20:47:28 +0200

    made cleanup script work

--- service/cleanup.sh
+++ service/cleanup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+validity_period=900
 while true; do
 	sqlite3 /service/instance/msp.sqlite "PRAGMA foreign_keys = ON; \
-		DELETE FROM user WHERE init_time+60<$(date +%s);"
+		DELETE FROM user WHERE init_time+$validity_period<$(date +%s);"
 	sleep 30
 done &
--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -11,12 +11,3 @@ source /service/cleanup.sh
 
 #gunicorn -w 4 -b '127.0.0.1:6969' 'managedServiceProvider:create_app()'
 gunicorn -w 4 -b '0.0.0.0:5000' 'managedServiceProvider:create_app()'
-
-#validity_period=60
-#while true; do
-#	sqlite3 "./instance/msp.sqlite" "DELETE FROM user WHERE $(current_epoch)-init_time > $(validity_period)"
-#	sqlite3 /service/instance/msp.sqlite "PRAGMA foreign_keys = ON; \
-#		DELETE FROM user WHERE init_time+60<$(date +%s);"
-#	/service/cleanup.sh
-#	sleep 30
-#done &
