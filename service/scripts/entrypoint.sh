367aa7753288189a427ca3ee44063ce38cd55fe7
maxiibon <maxerkel@web.de>
2023-07-18 13:32:46 +0200

    test

--- service/scripts/entrypoint.sh
+++ service/scripts/entrypoint.sh
@@ -8,5 +8,5 @@ flask --app managedServiceProvider init-db
 chmod 777 /service/instance/msp.sqlite
 source /service/scripts/cleanup.sh
 
-echo "$RANDOM+$RANDOM+$RANDOM" | shasum -a 256 | export FLASK_KEY=$(awk '{print $1}')
+#echo "$RANDOM+$RANDOM+$RANDOM" | shasum -a 256 | export FLASK_KEY=$(awk '{print $1}')
 gunicorn -c "config/gunicorn.conf.py" "managedServiceProvider:create_app()"
