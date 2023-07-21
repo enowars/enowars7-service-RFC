af9ac1b58b077e9ff7d7704a936427ed943035cb
Maximilian Erkel <maxerkel@web.de>
2023-07-21 10:31:22 +0200

    working unique keys

--- service/scripts/entrypoint.sh
+++ service/scripts/entrypoint.sh
@@ -3,16 +3,18 @@
 /usr/bin/nginx -c /etc/nginx/nginx.conf
 /etc/init.d/nginx start
 
+
 flask --app managedServiceProvider init-db
-
 chmod 777 /service/instance/msp.sqlite
-source /service/scripts/cleanup.sh
 
-if test -f "../instance/secret.txt"; then
+
+if test -f "/service/instance/secret.txt"; then
 	:
 else
-	echo "$RANDOM+$RANDOM+$RANDOM" | shasum -a 256 | awk '{print $1}' > "../instance/secret.txt"
-	chmod 555 ../instance/secret.txt
+	echo "$RANDOM+$RANDOM+$RANDOM" | shasum -a 256 | awk '{print $1}' > "/service/instance/secret.txt"
+	chmod 555 /service/instance/secret.txt
 fi
 
+
+source /service/scripts/cleanup.sh
 gunicorn -c "config/gunicorn.conf.py" "managedServiceProvider:create_app()"
