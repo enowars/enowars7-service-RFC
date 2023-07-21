072342a6183d83d0afcb8bb648b0ca8bb47295c2
Maximilian Erkel <maxerkel@web.de>
2023-07-21 10:56:30 +0200

    static key for signing

--- service/scripts/entrypoint.sh
+++ service/scripts/entrypoint.sh
@@ -8,13 +8,8 @@ flask --app managedServiceProvider init-db
 chmod 777 /service/instance/msp.sqlite
 
 
-if test -f "/service/instance/secret.txt"; then
-	:
-else
-	echo "$RANDOM+$RANDOM+$RANDOM" | shasum -a 256 | awk '{print $1}' > "/service/instance/secret.txt"
-	chmod 555 /service/instance/secret.txt
-fi
-
-
+source /service/scripts/gensecret.sh
 source /service/scripts/cleanup.sh
+
+
 gunicorn -c "config/gunicorn.conf.py" "managedServiceProvider:create_app()"
